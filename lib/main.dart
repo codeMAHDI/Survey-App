3d44b80b48f4c35de797543004d098d17c43cdba
Moshiur Rahman (Mahdi) <89650128+codeMAHDI@users.noreply.github.com>
2025-12-04 02:07:58 +0600

    Initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/survey_screen.dart';
 import 'package:flutter_firebase/upload_screen.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
